fe1e81320b55dbe4272d2149fe21edd47c7fc2f0
mojicsibro2 <abdullahiibraheem1996@gmail.com>
2025-10-11 23:24:50 -0700

    Cleaned up frontend branch - UI files only

--- Backend/src/shared/dto/api-response.dto.ts
+++
@@ -1,9 +0,0 @@
-export class ApiResponseDto<T> {
-  public message: string;
-  public data: T;
-
-  constructor(message: string = 'Successful', data: T) {
-    this.message = message;
-    this.data = data;
-  }
-}
--- Backend/src/shared/utils/helper.util.ts
+++
@@ -1,7 +0,0 @@
-export function isDevelopement(): boolean {
-  return process.env.NODE_ENV?.startsWith('dev') ? true : false;
-}
-
-export function isProduction(): boolean {
-  return process.env.NODE_ENV?.startsWith('prod') ? true : false;
-}
